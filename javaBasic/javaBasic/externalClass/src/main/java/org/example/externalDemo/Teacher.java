a340ca953fc06e82b0eb11959c16bf4a57b41d89
danny <danny@ridgesecurity.ai>
2023-01-25 15:02:46 +0000

    java basic

---
+++ javaBasic/javaBasic/externalClass/src/main/java/org/example/externalDemo/Teacher.java
@@ -0,0 +1,39 @@
+package org.example.externalDemo;
+
+public class Teacher {
+    private String name;
+    private int age;
+    private String teacherName;
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
